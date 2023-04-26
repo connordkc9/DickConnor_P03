bfd32f388b3c7df9c7b88d2c80690fe7f318b33d
Connor Dick <condkc9@gmail.com>
2023-04-26 17:40:03 -0500

    Create Powerup Scriptable Object Type

---
+++ Assets/Scripts/Powerup.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Powerup", menuName = "Powerup")]
+public class Powerup : ScriptableObject
+{
+    public string _name;
+    public string _description;
+
+    public Sprite _sprite;
+
+    public GameObject _object;
+}
